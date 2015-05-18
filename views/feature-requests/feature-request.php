6f1e5323ef511968da56462a183f9ab8d51f12e6
techprodigy <techprodigy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-18 04:02:45 +0000

    New Version Release
    git-svn-id: https://plugins.svn.wordpress.org/gallery-master/tags/1.0@1162420 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/feature-requests/feature-request.php
@@ -0,0 +1,113 @@
+<?php
+if (!is_user_logged_in())
+{
+	return;
+}
+else
+{
+	switch($gm_role)
+	{
+		case "administrator":
+			$user_role_permission = "manage_options";
+		break;
+
+		case "editor":
+			$user_role_permission = "publish_pages";
+		break;
+
+		case "author":
+			$user_role_permission = "publish_posts";
+		break;
+
+		case "contributor":
+			$user_role_permission = "edit_posts";
+		break;
+
+		case "subscriber":
+			$user_role_permission = "read";
+		break;
+	}
+	
+	if (!current_user_can($user_role_permission))
+	{
+		return;
+	}
+	else
+	{
+		?>
+		<div class="page-content-wrapper">
+			<div class="page-content">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="portlet box red-sunglo">
+							<div class="portlet-title">
+								<div class="caption">
+									<i class="icon-call-out"></i>
+									<?php _e("Feature Requests",gallery_master)?>
+								</div>
+							</div>
+							<div class="portlet-body form">
+								<form id="ux_frm_feature_requests">
+									<div class="form-body">
+										<div class="note note-warning">
+											<h4 class="block"><?php _e("Thank You!",gallery_master)?></h4>
+											<p><?php _e("Kindly fill in the below form, If you would like to suggest some features which are not in the Plugin.",gallery_master)?></p>
+											<p><?php _e("If you also have any suggestion/complaint, You can use the same form below.",gallery_master)?></p>
+											<p><?php _e("You can also write us on",gallery_master)?>
+												<a href="mailto:support@tech-prodigy.org" target="_blank">support@tech-prodigy.org</a>
+											</p>
+										</div>
+										<div class="row">
+											<div class="col-md-6">
+												<div class="form-group">
+													<label class="control-label"><?php _e("Your Name",gallery_master)?> :
+														<i class="icon-question tooltips" data-original-title="<?php _e("Please enter your Name which will be sent along with your Feature Request.", gallery_master)?>" data-placement="right"></i>
+														<span class="required" aria-required="true">*</span>
+													</label>
+													<div class="input-icon right">
+														<i class="fa"></i>
+														<input type="text" class="form-control" name="ux_txt_your_name" id="ux_txt_your_name" value="" placeholder="<?php _e("Enter your name here", gallery_master)?>">
+													</div>	
+												</div>
+											</div>
+											<div class="col-md-6">
+												<div class="form-group">
+													<label class="control-label"><?php _e("Your Email",gallery_master) ?> :
+														<i class="icon-question tooltips" data-original-title="<?php _e("Please enter your Email which will be sent along with your Feature Request.", gallery_master)?>" data-placement="right"></i>
+														<span class="required" aria-required="true">*</span>
+													</label>
+													<div class="input-icon right">
+														<i class="fa"></i>
+														<input type="text" class="form-control" name="ux_txt_email_address" id="ux_txt_email_address" value=""  placeholder="<?php _e("Enter your Email here", gallery_master)?>">
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="form-group">
+											<label class="control-label"><?php _e("Feature Request",gallery_master) ?> :
+												<i class="icon-question tooltips" data-original-title="<?php _e("Please enter your Feature Request which will be sent along.", gallery_master) ?>" data-placement="right"></i>
+												<span class="required" aria-required="true">*</span>
+											</label>
+											<div class="input-icon right">
+												<i class="fa"></i>
+												<textarea class="form-control" name="ux_txtarea_feature_request" id="ux_txtarea_feature_request" rows="8"  placeholder="<?php _e("Enter your Feature Request here", gallery_master)?>"></textarea>
+											</div>
+										</div>
+										<div class="line-separator"></div>
+										<div class="form-actions">
+											<div class="btn-set pull-right">
+												<button type="submit" class="btn red-sunglo"><?php _e("Send Request",gallery_master)?></button>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php
+	}
+}
+?>
---
+++ views/premium-editions/premium-editions.php
@@ -0,0 +1,290 @@
+<?php
+if (!is_user_logged_in())
+{
+	return;
+}
+else
+{
+	switch($gm_role)
+	{
+		case "administrator":
+			$user_role_permission = "manage_options";
+			break;
+		case "editor":
+			$user_role_permission = "publish_pages";
+			break;
+		case "author":
+			$user_role_permission = "publish_posts";
+			break;
+		case "contributor":
+			$user_role_permission = "edit_posts";
+			break;
+		case "subscriber":
+			$user_role_permission = "read";
+			break;
+	}
+
+	if (!current_user_can($user_role_permission))
+	{
+		return;
+	}
+	else
+	{
+		?>
+		<div class="page-content-wrapper">
+			<div class="page-content">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="portlet box red-sunglo">
+							<div class="portlet-title">
+								<div class="caption">
+									<i class="icon-basket"></i><?php _e("Premium Editions",gallery_master)?>
+								</div>
+							</div>
+							<div class="portlet-body form">
+								<form id="ux_frm_premium_editions">
+									<div class="form-body">
+										<div class="row margin-bottom-40 margin-top-10">
+											<div class="col-md-4">
+												<div class="pricing hover-effect">
+													<div class="pricing-head">
+														<h3><?php _e("Beginner",gallery_master)?>
+															<span><?php _e("For WordPress Beginners",gallery_master)?></span>
+														</h3>
+														<h4>
+															<i>$</i><i>3</i>
+															<span><?php _e("Per Month",gallery_master)?></span>
+														</h4>
+													</div>
+													<ul class="pricing-content list-unstyled">
+														<li>
+															<i class="icon icon-check"></i><?php _e("For Single Installation",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-close red-icon"></i><?php _e("Support Multisite",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Unlimited Galleries",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Unlimited Images",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Multi-Lingual",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Auto Plugin Updates",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Default Lightbox",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Restore Settings",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Purge Galleries",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("SEO Friendly",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Thumbnail Settings",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Support Media Manager",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Upload Videos",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-close red-icon"></i><?php _e("Bulk Deletion",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-close red-icon"></i><?php _e("Flip Images",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-close red-icon"></i><?php _e("Rotate Images",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-close red-icon"></i><?php _e("Copy Images to Other Galleries",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-close red-icon"></i><?php _e("Move Images to Other Galleries",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-close red-icon"></i><?php _e("Exclude Images from Galleries",gallery_master)?>
+														</li>
+													</ul>
+													<div class="pricing-footer">
+														<a target="_blank" href="http://tech-prodigy.org/product/gallery-master-beginner-edition/" class="btn red-sunglo">
+															<?php _e("Order Now",gallery_master)?> <i class="m-icon-swapright m-icon-white"></i>
+														</a>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="pricing pricing-active hover-effect">
+													<div class="pricing-head pricing-head-active">
+														<h3><?php _e("Pro",gallery_master)?>
+															<span><?php _e("For Wordpress Pro Users",gallery_master)?></span>
+														</h3>
+														<h4><i>$</i><i>5</i>
+															<span><?php _e("Per Month",gallery_master)?></span>
+														</h4>
+													</div>
+													<ul class="pricing-content list-unstyled">
+														<li>
+															<i class="icon icon-check"></i><?php _e("For Single Installation",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-close red-icon"></i><?php _e("Support Multisite",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Unlimited Galleries",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Unlimited Images",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Multi-Lingual",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Auto Plugin Updates",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Default Lightbox",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Restore Settings",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Purge Galleries",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("SEO Friendly",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Thumbnail Settings",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Support Media Manager",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Upload Videos",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Bulk Deletion",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Flip Images",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Rotate Images",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Copy Images to Other Galleries",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Move Images to Other Galleries",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Exclude Images from Galleries",gallery_master)?>
+														</li>
+													</ul>
+													<div class="pricing-footer">
+														<a target="_blank" href="http://tech-prodigy.org/product/gallery-master-pro-edition/" class="btn red-sunglo">
+															<?php _e("Oreder Now",gallery_master)?> <i class="m-icon-swapright m-icon-white"></i>
+														</a>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="pricing hover-effect">
+													<div class="pricing-head">
+														<h3><?php _e("Developer",gallery_master)?>
+															<span><?php _e("Upto 5 Websites for Pro Users",gallery_master)?></span>
+														</h3>
+														<h4><i>$</i><i>35</i>
+															<span><?php _e("Per Month",gallery_master)?></span>
+														</h4>
+													</div>
+													<ul class="pricing-content list-unstyled">
+														<li>
+															<i class="icon icon-check"></i><?php _e("For 10 Installations",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Support Multisite",gallery_master)?>
+															<span class="required" aria-required="true">*</span>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Unlimited Galleries",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Unlimited Images",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Multi-Lingual",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Auto Plugin Updates",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Default Lightbox",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Restore Settings",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Purge Galleries",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("SEO Friendly",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Thumbnail Settings",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Support Media Manager",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Upload Videos",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Bulk Deletion",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Flip Images",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Rotate Images",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Copy Images to Other Galleries",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Move Images to Other Galleries",gallery_master)?>
+														</li>
+														<li>
+															<i class="icon icon-check"></i><?php _e("Exclude Images from Galleries",gallery_master)?>
+														</li>
+													</ul>
+													<div class="pricing-footer">
+														<a target="_blank" href="http://tech-prodigy.org/product/gallery-master-developer-edition/" class="btn red-sunglo">
+															<?php _e("Order Now",gallery_master)?> <i class="m-icon-swapright m-icon-white"></i>
+														</a>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php
+	}
+}
+?>
